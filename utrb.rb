794445427c236b34148f7ce65cb771f7c47979eb
okisanjp <okisan.jp@gmail.com>
2016-12-09 18:37:08 +0900

    UPDATE hash

--- utrb.rb
+++ utrb.rb
@@ -3,7 +3,7 @@ require "formula"
 class Utrb < Formula
   homepage "https://github.com/okisanjp/homebrew-utrb"
   url "https://github.com/okisanjp/homebrew-utrb/archive/v1.0.tar.gz"
-  sha256 "606d00bc4736ef3fe10fdaa554985a08fd5642279a96f30ab2727b2cc7a771c1"
+  sha256 "49db403eef2c18c3cad4e2fa22ca604f48b14af38339b19ad84b71b4a0bedd98"
   head "https://github.com/okisanjp/homebrew-utrb.git"
   version "1.0"
 
