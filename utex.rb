6d50945deb28132bcdafe3beffda146913ec4d5f
okisanjp <okisan.jp@gmail.com>
2016-12-09 19:02:33 +0900

    UPDATE fomula

--- utex.rb
+++ utex.rb
@@ -2,10 +2,10 @@ require "formula"
 
 class Utex < Formula
   homepage "https://github.com/okisanjp/homebrew-utex"
-  url "https://github.com/okisanjp/homebrew-utex/archive/v1.1.tar.gz"
+  url "https://github.com/okisanjp/homebrew-utex/archive/v1.4.tar.gz"
   sha256 "51b784c9c7c7a6b87647d32342870fc92418a4cf2aef2975f39fa9246fce43c1"
   head "https://github.com/okisanjp/homebrew-utex.git"
-  version "1.1"
+  version "1.4"
 
   def install
     bin.install "utex"
