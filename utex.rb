40f5ca8e5d5110e7a76a52fbba5bd9afa21d0c9b
okisanjp <okisan.jp@gmail.com>
2016-12-09 18:58:11 +0900

    UPDATE hash

--- utex.rb
+++ utex.rb
@@ -3,7 +3,7 @@ require "formula"
 class Utex < Formula
   homepage "https://github.com/okisanjp/homebrew-utex"
   url "https://github.com/okisanjp/homebrew-utex/archive/v1.1.tar.gz"
-  sha256 "49db403eef2c18c3cad4e2fa22ca604f48b14af38339b19ad84b71b4a0bedd98"
+  sha256 "51b784c9c7c7a6b87647d32342870fc92418a4cf2aef2975f39fa9246fce43c1"
   head "https://github.com/okisanjp/homebrew-utex.git"
   version "1.1"
 
